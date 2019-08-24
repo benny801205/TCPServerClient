3c4bd22c8eecddab9782da540ef193d086f769c0
benny801205 <54221041+benny801205@users.noreply.github.com>
2019-08-24 00:51:28 -0600

    update

---
+++ Client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Scoket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverIP = "192.168.1.196";
+            int port = 8080;
+            TCPClient(port, serverIP);
+
+        }
+
+        public static void TCPClient(int port, string serverIP)
+        {
+            string msg = "hello";
+            TcpClient client = new TcpClient(serverIP, port);
+            int bytecount = Encoding.ASCII.GetByteCount(msg);
+            byte[] senddata = new byte[bytecount];
+            senddata = Encoding.ASCII.GetBytes(msg);
+            NetworkStream stream = client.GetStream();
+            stream.Write(senddata, 0, senddata.Length);
+            stream.Close();
+            client.Close();
+        }
+
+
+
+
+
+        public static void sendUserInfoToServer(int port, string serverIP )
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect(serverIP, port);
+            byte[] buffer = Encoding.ASCII.GetBytes("I'm from C#dedededededdefefefefefefefefggggggggggggggggggggggggggggggggggggggggg");
+            s.Send(buffer);
+            Console.WriteLine("snet");
+
+            //receive
+            byte[] rcvLenBytes = new byte[4];
+            s.Receive(rcvLenBytes);
+            int rcvLen = System.BitConverter.ToInt32(rcvLenBytes, 0);
+            byte[] rcvBytes = new byte[rcvLen];
+            s.Receive(rcvBytes);
+            String rcv = System.Text.Encoding.ASCII.GetString(rcvBytes);
+
+            Console.WriteLine("Client received: " + rcv);
+
+
+        }
+
+
+
+    }
+
+
+}
---
+++ Server.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerSocket
+{
+
+    class Program
+    {
+
+        // Main Method 
+        static void Main(string[] args)
+        {
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(IPAddress.Any, 8080);
+            TcpClient client = default(TcpClient);
+            Console.WriteLine(myIP());
+           // Console.ReadLine();
+            try
+            {
+                server.Start();
+                Console.WriteLine("server started");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+                byte[] recbuff = new byte[100];
+                NetworkStream stream = client.GetStream();
+                stream.Read(recbuff, 0, recbuff.Length);
+                string msg = Encoding.ASCII.GetString(recbuff);
+                Console.WriteLine("from client:"+msg);
+                stream.Close();
+                client.Close();
+
+
+            }
+
+
+
+
+
+
+        }
+
+
+        public static string myIP()
+        {
+            string localIP;
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+            socket.Connect("8.8.8.8", 65530);
+            IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+            localIP = endPoint.Address.ToString();
+            socket.Close();
+            return localIP;
+        }
+
+
+
+
+        public static void ExecuteServer()
+        {
+            // Establish the local endpoint  
+            // for the socket. Dns.GetHostName 
+            // returns the name of the host  
+            // running the application. 
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 59091);
+
+            // Creation TCP/IP Socket using  
+            // Socket Class Costructor 
+            Socket listener = new Socket(ipAddr.AddressFamily,
+                         SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                // Using Bind() method we associate a 
+                // network address to the Server Socket 
+                // All client that will connect to this  
+                // Server Socket must know this network 
+                // Address 
+                listener.Bind(localEndPoint);
+
+                // Using Listen() method we create  
+                // the Client list that will want 
+                // to connect to Server 
+                listener.Listen(10);
+                
+                while (true)
+                {
+
+                    Console.WriteLine("Waiting connection ... ");
+
+                    // Suspend while waiting for 
+                    // incoming connection Using  
+                    // Accept() method the server  
+                    // will accept connection of client 
+                    Socket clientSocket = listener.Accept();
+
+                    // Data buffer 
+                    byte[] bytes = new Byte[1024];
+                    string data = null;
+
+                    while (true)
+                    {
+
+                        int numByte = clientSocket.Receive(bytes);
+
+                        data += Encoding.ASCII.GetString(bytes,
+                                                   0, numByte);
+
+                        if (data.IndexOf("<EOF>") > -1)
+                            break;
+                    }
+
+                    Console.WriteLine("Text received -> {0} ", data);
+                    byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+                    // Send a message to Client  
+                    // using Send() method 
+                    clientSocket.Send(message);
+
+                    // Close client Socket using the 
+                    // Close() method. After closing, 
+                    // we can use the closed Socket  
+                    // for a new Client Connection 
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
